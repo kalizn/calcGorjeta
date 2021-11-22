15a6abfc87b5066ffab02f0820c5d21740fb6286
kalizn <netotads2019@gmail.com>
2021-11-22 10:59:30 -0300

    Base

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+import styled from 'styled-components/native';
 
-export default function App() {
+const Page = styled.SafeAreaView`
+  flex:1;
+  align-items:center;
+`;
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Page>
+
+    </Page>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
