402ecfbc5e1c82d9623e4d395738e6a241ebe45a
kalizn <netotads2019@gmail.com>
2021-11-22 11:27:01 -0300

    V.01

--- App.js
+++ App.js
@@ -7,10 +7,44 @@ const Page = styled.SafeAreaView`
   align-items:center;
 `;
 
+const HeaderText = styled.Text`
+  font-size:25px;
+`;
+
+const Input = styled.TextInput`
+  width:90%;
+  height:50px;
+  font-size:18px;
+  background-color: #EEE;
+  margin-top:20px;
+  border-radius:10px;
+  padding:10px;
+`;
+
+const CalcButton = styled.Button`
+  margin-top:10px;
+`;
+
+function calc(){
+  let nBill = parseFloat(bill);
+  setBill('');
+}
+
 export default () => {
+
+  const [bill, setBill] = useState('');
+
   return (
     <Page>
-
+      <HeaderText>Calculadora de Gorjeta</HeaderText>
+      <Input 
+        placeholder="Quanto deu a conta?"
+        placeholderTextColor="#000"
+        keyboardType="numeric"
+        value={bill}
+        onChangeText={ (n) => setBill(n) }
+      />
+      <CalcButton title="Calcular" onPress={calc} />
     </Page>
   );
 }
